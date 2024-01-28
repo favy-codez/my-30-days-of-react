16d4ed4d3f6896a3f1373bc781303368ddfb8f22
Ezeliora Godsfavour <phavourcode@gmail.com>
2024-01-28 07:34:49 +0100

    added day 2

--- my-30days-of-react/src/Pages/DAY2/Day2.jsx
+++ my-30days-of-react/src/Pages/DAY2/Day2.jsx
@@ -4,18 +4,48 @@ import Booklist from './Booklist';
 const Day2 = () => {
   const books = [
     {
-      author: 'Jordan Moore',
-      title: 'Interesting Facts For Curious Minds',
-      img: './images/book-1.jpg',
+      author: 'Chimamanda Ngozi Adichie',
+      title: 'Purple Hibiscus',
+      img: 'https://m.media-amazon.com/images/I/51gfnebRhsL._SY445_SX342_.jpg',
     },
     {
-      author: 'James Clear',
-      title: 'Atomic Habits',
-      img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+      author: 'Chimamanda Ngozi Adichie',
+      title: 'Americanah',
+      img: 'https://m.media-amazon.com/images/I/41b6InkMuZL._SY445_SX342_.jpg',
     },
+    {
+      author: 'Chimamanda Ngozi Adichie',
+      title: 'Half of a Yellow Sun',
+      img: 'https://m.media-amazon.com/images/I/41vYTyIgiGL._SY445_SX342_.jpg',
+    },
+    {
+      author: 'Chimamanda Ngozi Adichie',
+      title: 'We Should All Be Feminists ',
+      img: 'https://m.media-amazon.com/images/I/41-NznAOGjL._SY445_SX342_.jpg',
+    },
+    {
+      author: 'Chimamanda Ngozi Adichie',
+      title: 'The Thing Around Your Neck',
+      img: 'https://m.media-amazon.com/images/I/31pmTFDWyZL._SY445_SX342_.jpg',
+    },
+    {
+      author: 'Chimamanda Ngozi Adichie',
+      title: 'Dear Ijeawele, or A Feminist Manifesto in Fifteen Suggestions',
+      img: 'https://m.media-amazon.com/images/I/51YZWJvOGkL._SY445_SX342_.jpg',
+    }
   ];
 
-  return <Booklist/>;
+  return (
+    <>
+      <h1 className='day1header'>DAY 2</h1>
+      <p className='day1text'>Learnt how to use</p>
+      <section className='booklist'>
+        <Booklist/>
+        <Booklist/>
+        <Booklist/>
+      </section>
+    </>
+  );
 }
 
 export default Day2
--- my-30days-of-react/src/Pages/Footer.jsx
+++ my-30days-of-react/src/Pages/Footer.jsx
@@ -3,12 +3,133 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div>
-        <div className="">
-            <Link to="/Day1" >DAY1</Link>
-            <Link to="/Day2" >DAY2</Link>
+    <>
+        <div className="footer">
+        <div className="footerB">
+          <Link to="/">
+            <button>Home</button>
+          </Link>
         </div>
-    </div>
+
+            <div className="footerA">
+            <div className="foot-sec">
+              <Link to="/Day1" >
+                <p>DAY 1</p>
+              </Link>
+              <Link to="/Day2" >
+                <p>DAY 2</p>
+              </Link>
+              <Link to="/Day2" >
+                <p>DAY 3</p>
+              </Link>
+              <Link to="/Day2" >
+                <p>DAY 4</p>
+              </Link>
+              <Link to="/Day2" >
+                <p>DAY 5</p>
+              </Link>
+            </div>
+
+            <div className="foot-sec">
+              <Link to="/Day1" >
+                <p>DAY 6</p>
+              </Link>
+              <Link to="/Day2" >
+                <p>DAY 7</p>
+              </Link>
+              <Link to="/Day2" >
+                <p>DAY 8</p>
+              </Link>
+              <Link to="/Day2" >
+                <p>DAY 9</p>
+              </Link>
+              <Link to="/Day2" >
+                <p>DAY 10</p>
+              </Link>
+            </div>
+
+            <div className="foot-sec">
+              <Link to="/Day1" >
+                <p>DAY 11</p>
+              </Link>
+              <Link to="/Day2" >
+                <p>DAY 12</p>
+              </Link>
+              <Link to="/Day2" >
+                <p>DAY 13</p>
+              </Link>
+              <Link to="/Day2" >
+                <p>DAY 14</p>
+              </Link>
+              <Link to="/Day2" >
+                <p>DAY 15</p>
+              </Link>
+            </div>
+
+            <div className="foot-sec">
+              <Link to="/Day1" >
+                <p>DAY 16</p>
+              </Link>
+              <Link to="/Day2" >
+                <p>DAY 17</p>
+              </Link>
+              <Link to="/Day2" >
+                <p>DAY 18</p>
+              </Link>
+              <Link to="/Day2" >
+                <p>DAY 19</p>
+              </Link>
+              <Link to="/Day2" >
+                <p>DAY 20</p>
+              </Link>
+            </div>
+
+            <div className="foot-sec">
+              <Link to="/Day1" >
+                <p>DAY 21</p>
+              </Link>
+              <Link to="/Day1" >
+                <p>DAY 22</p>
+              </Link>
+              <Link to="/Day2" >
+                <p>DAY 23</p>
+              </Link>
+              <Link to="/Day2" >
+                <p>DAY 24</p>
+              </Link>
+              <Link to="/Day2" >
+                <p>DAY 25</p>
+              </Link>
+            </div>
+
+            <div className="foot-sec">
+              <Link to="/Day1" >
+                <p>DAY 26</p>
+              </Link>
+              <Link to="/Day1" >
+                <p>DAY 27</p>
+              </Link>
+              <Link to="/Day1" >
+                <p>DAY 28</p>
+              </Link>
+              <Link to="/Day1" >
+                <p>DAY 29</p>
+              </Link>
+              <Link to="/Day1" >
+                <p>DAY 30</p>
+              </Link>
+            </div>
+            </div>
+
+            <hr className='divider'/>
+
+            <div className="footerC">
+              <Link to="/">
+                <p>Github</p>
+              </Link>
+            </div>
+        </div>
+    </>
   )
 }
 
